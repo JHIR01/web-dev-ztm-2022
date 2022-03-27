92297782cef0862a47a7425e16974b39f9cda1a4
Jacob-Henry <henryirivere@gmail.com>
2022-03-27 14:03:25 -0400

    for now
    just commiting

--- section-13/es5-and-es6/script.js
+++ section-13/es5-and-es6/script.js
@@ -20,3 +20,52 @@ const obj = {
     wizardLevel: false
 }
 
+const player = obj.player;
+const experience = obj.experience;
+let wizardLevel = obj.wizardLevel;
+
+// with destructuring you can do this instead
+const { player, experience } = obj;
+let { wizardLevel } = obj;
+
+const name = "john snow";
+
+// TEMPLATE STRINGS
+
+const greeting = "Hello " + user + " you seem to be doing " + greet + "."
+
+const greetingBest = `"Hello there"`; // the `` will display everthing written inbetween them
+
+const newName = "Sally";
+const age = 34;
+const pet = "horse";
+
+const betterGreeting = `Hello ${newName} you seem to be ${age - 10}. What a lovely ${pet} you have!`; // the new syntax will refer to the item being called within
+// the brackets 
+
+
+// DEFAULT ARGUMENTS
+
+function greet(name="", age=30, pet="cat"){
+    return `Hello ${newName} you seem to be ${age - 10}. What a lovely ${pet} you have!`;
+}
+// because of the arguements in the parameters, the values assigned there are automatically placed.
+// if someone does not enter in different arguements, then the function will not fail
+
+// SYMBOL
+
+// symbols are used to create completely new and unique items
+// when compared to each other, they will never be the same
+
+let sym1 = Symbol();
+let sym2 = Symbol('foo');
+let sym3 = Symbol('foo');
+
+// ARROW FUNCTIONS
+
+function add(a, b){
+    return a + b;
+}
+
+// instead of writing function, you just need to write an arrow
+const add = (a, b) => a + b;
